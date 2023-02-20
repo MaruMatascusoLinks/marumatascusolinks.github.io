1afc86d8db3ffdc78db96d1418ce06663f2541ac
Matascuso <mvmatascuso@gmail.com>
2023-02-20 19:42:49 -0300

    agrego javascript y me trabo

--- js/todolist.js
+++ js/todolist.js
@@ -1,7 +1,35 @@
+/* ------- click en icono agregar --------- */
 
-let master = document.getElementById('container')
-let addButton = document.getElementById('add').addEventListener('click', function(){
-    let txt = "Holi soy una tarea"
-    let modelo = `<li class="list-group-item">${txt}</li>`
-    master.innerHTML += modelo
+let bienvenida = document.getElementById('container-bienvenida')
+let plus  = document.getElementById('plus-img')
+
+let agregar = document.getElementById('nuevatarea').addEventListener('click', function(event){
+    event.preventDefault()
+    bienvenida.classList.toggle('inactive')
+    plus.classList.toggle('inactive')
+    document.getElementById('container-input').style.display = 'block'
 })
+
+/* Clic en nueva tarea */
+
+let newtask = document.getElementById('container-newtask')
+let addBtn = document.getElementById('add').addEventListener('click', function(event){
+    event.preventDefault()
+    newtask.classList.toggle('active')
+    document.getElementById('container-input').style.display = 'none'
+})
+
+/* clic en listo */
+let listadito =  document.getElementById('ul-input')
+let txt = "acá debería agarrar el input de tarea"
+
+let listo = document.getElementById('listo').addEventListener('click', function(event){
+    event.preventDefault()
+    newtask.classList.toggle('inactive')
+    document.getElementById('container-input').style.display = 'block'
+    let modelo = `<li class="list-group-item active" aria-current="true">${txt}</li>`
+    listadito.innerHTML += modelo
+})
+
+
+
